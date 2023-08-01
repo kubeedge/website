0345e3bb08a745ed58ff251731bf38ec92450e5b
Ansh Goyal <anshgoyal1704@gmail.com>
2023-08-01 09:47:34 +0530

    Add Showcase Page
    Signed-off-by: Ansh Goyal <anshgoyal1704@gmail.com>

---
+++ src/data/users.tsx
@@ -0,0 +1,230 @@
+/* eslint-disable global-require */
+
+import {translate} from '@docusaurus/Translate';
+import {sortBy} from '@site/src/utils/jsUtils';
+// LIST OF AVAILABLE TAGS
+// Available tags to assign to a showcase site
+// Please choose all tags that you think might apply.
+// We'll remove inappropriate tags, but it's less likely that we add tags.
+export type TagType =
+  // DO NOT USE THIS TAG: we choose sites to add to favorites
+  | 'favorite'
+  // For open-source sites, a link to the source code is required.
+  // The source should be the *website's* source, not the project's source!
+  | 'opensource'
+  | 'product'
+  // Feel free to add the 'design' tag as long as there's _some_ level of
+  // CSS/swizzling.
+  | 'design'
+  // Site must have more than one locale.
+  | 'i18n'
+  | 'versioning'
+  // Large sites are defined as those with > 200 pages, excluding versions.
+  | 'large'
+  | 'meta'
+  | 'personal'
+  // Right-to-left direction.
+  | 'rtl';
+
+// Add sites to this list
+// prettier-ignore
+const Users: User[] = [
+  {
+    title: 'AgileTs',
+    description: 'Global State and Logic Framework for reactive Applications',
+    preview: require('./showcase/agilets.png'),
+    website: 'https://agile-ts.org/',
+    source: 'https://github.com/agile-ts/documentation',
+    tags: ['opensource', 'design'],
+  },
+  {
+    title: 'AI-Speaker',
+    description: 'Local, reliable, fast and private Audio and IoT gate.',
+    preview: require('./showcase/agilets.png'),
+    website: 'https://ai-speaker.com/',
+    source: 'https://github.com/sviete/AIS-WWW',
+    tags: ['opensource'],
+  },
+  {
+    title: 'Aide Jeune',
+    description:
+      'French Discord server that helps young people who have been bullied or feel bad about themselves',
+    preview: require('./showcase/agilets.png'),
+    website: 'https://aidejeune.fr',
+    source: null,
+    tags: [],
+  },
+  {
+    title: "Akara's blog",
+    description: 'Personal frontend blog for learning',
+    preview: require('./showcase/agilets.png'),
+    website: 'https://messiahhh.github.io/blog/',
+    source: 'https://github.com/messiahhh/blog',
+    tags: ['opensource', 'personal'],
+  },
+  {
+    title: 'Algolia DocSearch',
+    description:
+      'The best search experience for docs, integrates in minutes, for free',
+    preview: require('./showcase/agilets.png'),
+    website: 'https://docsearch.algolia.com/',
+    source: 'https://github.com/algolia/docsearch/tree/main/packages/website',
+    tags: ['favorite', 'opensource', 'product'],
+  },
+  {
+    title: 'Apache APISIX',
+    description:
+      'A Dynamic, Real-Time, High-Performance Cloud-Native API Gateway',
+    preview: require('./showcase/agilets.png'),
+    website: 'https://apisix.apache.org/',
+    source: 'https://github.com/apache/apisix-website',
+    tags: ['opensource', 'i18n', 'large', 'favorite'],
+  },
+  {
+    title: 'ShellHub Documentation',
+    description: 'A centralized SSH server for the the edge and cloud computing',
+    preview: null,
+    website: 'https://docs.shellhub.io/',
+    source: 'https://github.com/shellhub-io/docs',
+    tags: ['opensource','product'],
+  },
+  {
+    title: 'Dynamoose',
+    description: 'A modeling tool for Amazon\'s DynamoDB',
+    preview: null,
+    website: 'https://dynamoosejs.com/',
+    source: 'https://github.com/dynamoose/dynamoose/tree/main/docs',
+    tags: ['opensource', 'favorite'],
+  },
+
+  /*
+  Pro Tip: add your site in alphabetical order.
+  Appending your site here (at the end) is more likely to produce Git conflicts.
+   */
+];
+
+export type User = {
+  title: string;
+  description: string;
+  preview: string | null; // null = use our serverless screenshot service
+  website: string;
+  source: string | null;
+  tags: TagType[];
+};
+
+export type Tag = {
+  label: string;
+  description: string;
+  color: string;
+};
+
+export const Tags: {[type in TagType]: Tag} = {
+  favorite: {
+    label: translate({message: 'Favorite'}),
+    description: translate({
+      message:
+        'Our favorite Docusaurus sites that you must absolutely check out!',
+      id: 'showcase.tag.favorite.description',
+    }),
+    color: '#e9669e',
+  },
+
+  opensource: {
+    label: translate({message: 'Open-Source'}),
+    description: translate({
+      message: 'Open-Source Docusaurus sites can be useful for inspiration!',
+      id: 'showcase.tag.opensource.description',
+    }),
+    color: '#39ca30',
+  },
+
+  product: {
+    label: translate({message: 'Product'}),
+    description: translate({
+      message: 'Docusaurus sites associated to a commercial product!',
+      id: 'showcase.tag.product.description',
+    }),
+    color: '#dfd545',
+  },
+
+  design: {
+    label: translate({message: 'Design'}),
+    description: translate({
+      message:
+        'Beautiful Docusaurus sites, polished and standing out from the initial template!',
+      id: 'showcase.tag.design.description',
+    }),
+    color: '#a44fb7',
+  },
+
+  i18n: {
+    label: translate({message: 'I18n'}),
+    description: translate({
+      message:
+        'Translated Docusaurus sites using the internationalization support with more than 1 locale.',
+      id: 'showcase.tag.i18n.description',
+    }),
+    color: '#127f82',
+  },
+
+  versioning: {
+    label: translate({message: 'Versioning'}),
+    description: translate({
+      message:
+        'Docusaurus sites using the versioning feature of the docs plugin to manage multiple versions.',
+      id: 'showcase.tag.versioning.description',
+    }),
+    color: '#fe6829',
+  },
+
+  large: {
+    label: translate({message: 'Large'}),
+    description: translate({
+      message:
+        'Very large Docusaurus sites, including many more pages than the average!',
+      id: 'showcase.tag.large.description',
+    }),
+    color: '#8c2f00',
+  },
+
+  meta: {
+    label: translate({message: 'Meta'}),
+    description: translate({
+      message: 'Docusaurus sites of Meta (formerly Facebook) projects',
+      id: 'showcase.tag.meta.description',
+    }),
+    color: '#4267b2', // Facebook blue
+  },
+
+  personal: {
+    label: translate({message: 'Personal'}),
+    description: translate({
+      message:
+        'Personal websites, blogs and digital gardens built with Docusaurus',
+      id: 'showcase.tag.personal.description',
+    }),
+    color: '#14cfc3',
+  },
+
+  rtl: {
+    label: translate({message: 'RTL Direction'}),
+    description: translate({
+      message:
+        'Docusaurus sites using the right-to-left reading direction support.',
+      id: 'showcase.tag.rtl.description',
+    }),
+    color: '#ffcfc3',
+  },
+};
+
+export const TagList = Object.keys(Tags) as TagType[];
+function sortUsers() {
+  let result = Users;
+  // Sort by site name
+  result = sortBy(result, (user) => user.title.toLowerCase());
+  // Sort by favorite tag, favorites first
+  result = sortBy(result, (user) => !user.tags.includes('favorite'));
+  return result;
+}
+
+export const sortedUsers = sortUsers();
